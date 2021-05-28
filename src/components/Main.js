1930370e675e72e6c7bc5f0f100e0e22987cd1c5
lakshitmadaan20 <51285500+lakshitmadaan20@users.noreply.github.com>
2021-05-28 23:24:05 +0530

    updates

--- src/components/Main.js
+++ src/components/Main.js
@@ -19,15 +19,20 @@ class Main extends Component {
         unsiswap: null,
         loading: false,
         tokenAddress: [],
-        tokenAmount: []
+        tokenAmount: [],
+        approve: false
       }
     }
 
-    componentWillMount() {
-        this.loadWeb3()
-        this.loadBlockchainData()
+    async componentWillMount() {
+      //detect metamask
+      const metamaskInstalled = typeof window.web3 !== 'undefined'
+      this.setState({ metamaskInstalled })
+      if (metamaskInstalled) {
+        await this.loadWeb3()
+        await this.loadBlockchainData()
+      }
     }
-
     async loadWeb3() {
         if (window.ethereum) {
           window.web3 = new Web3(window.ethereum)
@@ -55,64 +60,79 @@ class Main extends Component {
         console.log(networkId)
     
         if (networkId === 3) {
-        const swap = new web3.eth.Contract(Swap, "0x0BEBDD9BA9e7fB68b6b658F159FCCbC963dAbD63")
+        const swap = new web3.eth.Contract(Swap, "0x2E73f59BA9BAdda63A997B85840EcB4d2E4ec77A")
         this.setState({ swapping: swap })
     
         const uniswap = new web3.eth.Contract(IUniswapV2Router02, "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D")
         this.setState({ uniswap: uniswap })
         
-        const erctoken = new web3.eth.Contract(IERC20, "0xad6d458402f60fd3bd25163575031acdce07538d")
-        this.setState({ ierc20: erctoken })
-          
-        const ercContract = new web3.eth.Contract(ERC20, "0xad6d458402f60fd3bd25163575031acdce07538d")
-        this.setState({ erc20: ercContract })  
-        
-        console.log(this.state.erc20, this.state.swapping,this.state.ierc20, this.state.uniswap, this.state.account)
+        console.log(this.state.swapping,this.state.uniswap, this.state.account)
         } else {
           alert("Smart contract not deployed to this network..")
         }
   }
 
- 
-  
-  async swapToken(targetToken, tokenVal, tokenAdd, tokenAmnt) {
-    await this.state.erc20.methods.approve("0x0BEBDD9BA9e7fB68b6b658F159FCCbC963dAbD63", tokenVal).send({from: this.state.account})
+  async onApprove(address, value) {
+    const web3 = window.web3
+    console.log(address,value)                                                    
+
+    const ercContract = await new web3.eth.Contract(ERC20, address)
+    this.setState({ erc20: ercContract })
+
+    console.log(address, value, this.state.erc20)
+    await this.state.erc20.methods.approve("0x2E73f59BA9BAdda63A997B85840EcB4d2E4ec77A", value).send({ from: this.state.account })
       .then(() => {
-        console.log(tokenAdd, tokenAmnt, targetToken)
-        this.state.swapping.methods.swap(tokenAdd, tokenAmnt, targetToken).send({ from: this.state.account })
-          .then((res) => {
-            console.log(res)
-          }).catch((error) => {
-        console.log(error)
+       this.setState({approve: true})
       })
-      }).catch((error) => {
-      console.log(error)
+      .catch(() => {
+        alert("Transaction failed")
       })
   }
+  
+  async swapToken(tokenInputAddress, tokenInputAmount, targetToken) {
+    if (this.state.approve) {
+      console.log(tokenInputAddress, tokenInputAmount, targetToken)
+      this.state.swapping.methods.swap(tokenInputAmount, tokenInputAddress, targetToken).send({ from: this.state.account })
+        .then((res) => {
+          console.log(res)
+          alert("Transaction Done Successfully")
+        })
+        .catch((error) => {
+      console.log(error)
+    }) 
+    } else {
+      alert("First Approve the transaction")
+    }
+  } 
       
     render() {
         return (
-            <div>
-                <Header accounts={this.state.account} balances={this.state.balance} />
+            <div className="container">
+            {this.state.metamaskInstalled ? 
+              <>
+              <Header accounts={this.state.account} balances={this.state.balance} />
                 <hr />
                 <div>
-              <form onSubmit={(event) => {
+               <form onSubmit={(event) => {
                 event.preventDefault()
+
                 const tokenVal = this.amount.value
-                const tokenAddress = this.state.tokenAddress.push(this.address.value)
-                const tokenValue = this.state.tokenAmount.push(this.amount.value)
+                const tokenAdd = this.address.value
                 const targetToken = this.targetAddress.value
+                const tokenAddress = this.state.tokenAddress.push(tokenAdd)
+                const tokenValue = this.state.tokenAmount.push(tokenVal)
                 this.setState({ tokenAddress: tokenAddress, tokenAmount: tokenValue })
-                const tokenAdd = this.state.tokenAddress
-                const tokenAmnt = this.state.tokenAmount
-                console.log(tokenVal,targetToken,tokenAdd, tokenAmnt)
-                this.swapToken(targetToken, tokenVal, tokenAdd, tokenAmnt)
-                }}>
+                const tokenInputAddress = this.state.tokenAddress
+                const tokenInputAmount = this.state.tokenAmount
+                console.log(tokenInputAddress, tokenInputAmount, targetToken)
+                this.swapToken(tokenInputAddress, tokenInputAmount, targetToken)
+                }
+              }>
                   <div className="form-group row">
                     <label className="col-sm-2 col-form-label">Token Address</label>
                     <div className="col-sm-10">
                     <input
-                     value="0xad6d458402f60fd3bd25163575031acdce07538d"
+                     required
                      ref={(input) => {this.address = input}}
                      type="text"
                      className="form-control"
@@ -124,7 +144,7 @@ class Main extends Component {
                     <label className="col-sm-2 col-form-label">Token Amount</label>
                     <div className="col-sm-10">
                     <input
-                    type="Number"
+                    required
                     className="form-control"
                     placeholder="Enter amount of tokens eg.1000"
                     ref={(input) => {this.amount = input}}
@@ -136,18 +156,36 @@ class Main extends Component {
                     <label className="col-sm-2 col-form-label">Target Token Address</label>
                     <div className="col-sm-10">
                     <input
+                    required
                     type="text"
-                    value="0x7b2810576aa1cce68f2b118cef1f36467c648f92"
                     className="form-control"
                     placeholder="Enter target token address"
                     ref={(input) => {this.targetAddress = input}}
                     />
                     </div>
-                    </div>
-                    <br/>        
-                    <button className="btn btn-outline-success">Submit</button>
-                </form>
                 </div>
+                <br/>
+                <button className="btn btn-success">Swap</button>
+                </form>
+            </div>
+            <br />
+            <div>
+              <hr />
+              <p>{this.state.approve ? <div className="text-success">Approved</div> : <div className="text-danger">Not Approved</div>}</p>
+              <button className="btn btn-primary" onClick={(event) => {
+                  event.preventDefault()
+                
+                  const address = this.address.value
+                  const value = this.amount.value
+                  console.log(address, value)
+                  this.onApprove(address, value)
+                }}>Approve</button>
+                </div>
+                </>  : <div>
+                  <h3 className="text-center">Please Install Metamask</h3>
+                </div>
+              
+            }
             </div>
         )
     }
